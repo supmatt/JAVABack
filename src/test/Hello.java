425f87dcf10609441c0bca20e3b9b3daa4645a7e
supmatt <sechenxi@hotmail.com>
2019-01-26 19:30:47 -0500

    Date

--- src/test/Hello.java
+++ src/test/Hello.java
@@ -1,5 +1,10 @@
 package test;
 
-public class Hello {
+import java.util.Date;
 
+public class Hello {
+	public static void main(String[] args) {
+		Date d1 = new Date();
+		System.out.println(");
+	}
 }
