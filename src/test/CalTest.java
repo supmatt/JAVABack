f09a78c1a823ad6d4dc2e6b9a48352ffef5e0e4c
supmatt <sechenxi@hotmail.com>
2019-02-05 15:45:33 -0500

    basicly use Calendar

--- src/test/CalTest.java
+++ src/test/CalTest.java
@@ -8,12 +8,30 @@ public class CalTest {
 	private static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+//		Calendar c = Calendar.getInstance();
+//		Date d = c.getTime();
+//		Date d2 = new Date(0);
+//		c.setTime(d2);
+//		System.out.println(sdf.format(d));
 		Calendar c = Calendar.getInstance();
-		Date d = c.getTime();
-		Date d2 = new Date(0);
-		c.setTime(d2);
-		System.out.println(d);
+		Date now = c.getTime();//time of now
+		System.out.println(now);// print time of this time
 		
+		c.setTime(now);
+		c.add(Calendar.MONTH, 1);
+		System.out.println(c.getTime());//time of next month
+		
+		c.setTime(now);
+		c.add(Calendar.YEAR, -1);
+		System.out.println(c.getTime());//time of last year
+		
+		c.setTime(now);
+		c.add(Calendar.MONTH, -1);
+		c.set(Calendar.DATE, 3);
+		System.out.println(c.getTime());//3ere day of last month
+		System.out.println(format(c.getTime()));
 	}
-
+		private static String format(Date time) {
+			return sdf.format(time);
+		}
 }
