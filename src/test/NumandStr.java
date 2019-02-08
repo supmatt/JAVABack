5c8c65bd9aa28543683c50c6c0547fbe66ad487e
supmatt <sechenxi@hotmail.com>
2019-02-07 21:00:52 -0500

    new project about number and string

---
+++ src/test/NumandStr.java
@@ -0,0 +1,15 @@
+package test;
+
+public class NumandStr {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 5;
+		Integer it = new Integer(i);
+//		int i2 = it.intValue();
+//		System.out.println(i2);
+		System.out.println(it instanceof Number);
+		
+	}
+
+}
