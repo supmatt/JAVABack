348d547094732c802f549a42b5144876592dcb2f
supmatt <sechenxi@hotmail.com>
2019-02-10 15:26:24 -0500

    Study use Math.*

--- src/test/CloseBox.java
+++ src/test/CloseBox.java
@@ -4,20 +4,54 @@ public class CloseBox {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int i = 5;
-		Integer it = new Integer(i);
-		int i2 = it.intValue();
-		int i3 = it;
-		System.out.println(i2);
-		System.out.println(it instanceof Number);
-		System.out.println(i3);
-		System.out.println(Integer.MAX_VALUE);
-		System.out.println(Integer.MIN_VALUE);
-		System.out.println("--------------");
-		byte b = 25;
-		Byte bb = new Byte(b);
-		byte b2 = bb.byteValue();
-		System.out.println(b2);
+		//Class1
+//		int i = 5;
+//		Integer it = new Integer(i);
+//		int i2 = it.intValue();
+//		int i3 = it;
+//		System.out.println(i2);
+//		System.out.println(it instanceof Number);
+//		System.out.println(i3);
+//		System.out.println(Integer.MAX_VALUE);
+//		System.out.println(Integer.MIN_VALUE);
+//		System.out.println("--------------");
+//		byte b = 25;
+//		Byte bb = new Byte(b);
+//		byte b2 = bb.byteValue();
+//		System.out.println(b2);
+		//Class2
+//		int i = 5;
+//		String str = String.valueOf(i);
+//		System.out.println(str);
+//		Integer it = i;
+//		String str2 = it.toString();
+//		System.out.println(str2);
+//		String str = "101";
+//		int i = Integer.parseInt(str);
+//		System.out.println(i);
+//		float f = 3.14f;
+//		System.out.println(f);
+//		String cc = String.valueOf(f);
+//		//facon2
+//		Float ft = f;
+//		String str2 = ft.toString();
+//		System.out.println("facon 2 is: " + str2);
+//		System.out.println(cc);
+//		float ff = Float.parseFloat(cc);
+//		System.out.println(ff);
+		//class3
+		float f1 = 5.4f;
+		float f2 = 5.5f;
+		System.out.println(Math.round(f1));
+		System.out.println(Math.round(f2));
+		System.out.println(Math.random());
+		System.out.println((int)(Math.random()*10));
+		System.out.println(Math.sqrt(9));
+		System.out.println(Math.pow(2, 4));
+		System.out.println(Math.PI);
+		System.out.println(Math.E);
+		
+		
 	}
 
 }
